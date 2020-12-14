749411d2af30cd5853cc1fd9f75b96dc34828ffe
nikxtaco <nikitamenon2510@gmail.com>
2020-12-14 15:39:46 +0530

    update

--- src/Page2/list.js
+++ src/Page2/list.js
@@ -4,53 +4,52 @@ import Navbar from "../components/transition1/navbar/navbar.js"
 
 const AllTimeFavorites = () => {
 
+  const [articles, setArticles] = useState([]);
+
+  /* Keeps track of whether a particular article has been selected */
   const [post_title, setPost_title] = useState("")
-  // const [post_id] = useState(0)
 
   const [show_post, setShow_post] = useState(0)
 
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true); // just use a background scheduler
 
-  function getArticles() {
-    fetch('https://flask-heroku-backend.herokuapp.com/get-top-news-articles/')
-      .then(response => response.json())
-      .then(json => {
-        setArticles(json);
-        setLoading(false);
-        console.log(json)
-      })
-  }
-
-  useEffect(() => {
-    getArticles();
-  }, [loading])
+  /* Makes sure the page scrolls to the top when switching from article to article */
 
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [post_title])
 
-  // function postArticle(data) {
+  /* Gets the whole list of articles from the back end just once when the page loads */
+
+  useEffect(() => {
+    fetch('/get-top-news-articles/') //https://flask-heroku-backend.herokuapp.com/get-top-news-articles/
+      .then(response => response.json())
+      .then(json => {
+        setArticles(json);
+        // setLoading(false); 
+        console.log(json)
+      })
+  }, []) //loading
+
+    /*  */
+
+  function postArticle(data) {
       
-  //   fetch('/post-selected-news-article/', 
-  //     {
-  //       method: 'POST',
-  //     headers: { 'Content-Type': 'application/json' },
-  //     body: JSON.stringify(data)
-  // })
-  //       .then(res => res.json())
-  //       .then(res => console.log(res))
-  //       // .then(data => setPostId(data.id));
-  // }
+    fetch('/post-selected-news-article/', 
+    {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    })
+        .then(res => res.json())
+        .then(res => console.log(res))
+        // .then(data => setPostId(data.id));
+  }
 
 //   useEffect(() => {
-//     // POST request using axios inside useEffect React hook
-//     const article = { title: 'React Hooks POST Request Example' };
-//     axios.post('https://reqres.in/api/articles', article)
-//         .then(response => console.log(response.data.id));
-
-// // empty dependency array means this effect will only run once (like componentDidMount in classes)
-// }, []);
+//     if(post_title!=="")
+//       postArticle(selected_article)
+// }, [post_title,selected_article]);
 
   const posts = articles.map(function(data, id) {
     return (
@@ -69,7 +68,7 @@ const AllTimeFavorites = () => {
       {
         return(
           <div key={id}>
-
+            {postArticle(data)}
             <br/>
             <h3 className="individual_post_title">{data.title}</h3> 
             
@@ -117,11 +116,11 @@ const AllTimeFavorites = () => {
 
       <div className="main_container">
         
-        <h5 className="blog_text_red back" onClick={() => {setPost_title(0)}}>Back To List</h5>
+        <h5 className="blog_text_red back" onClick={() => {setPost_title("")}}>Back To List</h5>
 
         {post}
 
-        <h5 className="back_to_list" onClick={() => {setPost_title(0)}}>
+        <h5 className="back_to_list" onClick={() => {setPost_title("")}}>
           Back To List
         </h5>
 
